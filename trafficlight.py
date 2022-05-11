ce0960097a5842f42496068b9705986060e842bc
NurulIzzati06 <105358666+NurulIzzati06@users.noreply.github.com>
2022-05-11 19:31:03 +0800

    test button

---
+++ trafficlight.py
@@ -0,0 +1,19 @@
+from gpiozero import Button
+
+button = Button(21)
+
+while True:
+    print(button.is_pressed)
+    while True:
+    if button.is_pressed:
+        print("Hello")
+    else:
+        print("Goodbye")
+        while True:
+            
+    button.wait_for_press()
+    print("Pressed")
+    button.wait_for_release()
+    print("Released")
+
+
