1d92d325e8b02e99725325d2c5c9b8cef620cc51
NurulIzzati06 <105358666+NurulIzzati06@users.noreply.github.com>
2022-05-11 21:21:17 +0800

    test button

--- trafficlight.py
+++ trafficlight.py
@@ -8,40 +8,40 @@ buzzer = Buzzer(15)
 while True:
     print(button.is_pressed)
     
-    while True:
-        button.wait_for_press()
+    button.wait_for_press()
         
-        if button.is_pressed:
-            print("Button pressed.")
-            
-            # green on
-            lights.green.on()
-            sleep (1)
-            
-            # green off
-            lights.green.off()
-            sleep(1)
-            
-            # amber on
-            lights.amber.on()
-            sleep(1)
-            
-            # amber off
-            lights.amber.off()
-            sleep(1)
-            
-            # red, buzzer on
-            lights.red.on()
-            buzzer.on()
-            sleep (3)
-            
-            # all light & buzzer off
-            lights.red.off()
-            lights.off()
-            buzzer.off()
-            delay(3)
-        else:
-            print("Goodbye")
+    if button.is_pressed:
+        print("Button pressed.")
+        
+        # green on
+        lights.green.on()
+        sleep (1)
+        
+        # green off
+        lights.green.off()
+        sleep(1)
+        
+        # amber on
+        lights.amber.on()
+        sleep(1)
+        
+        # amber off
+        lights.amber.off()
+        sleep(1)
+        
+        # red, buzzer on
+        lights.red.on()
+        buzzer.on()
+        sleep (3)
+        
+        # all light & buzzer off
+        lights.red.off()
+        lights.off()
+        buzzer.off()
+        delay(3)
+    else:
+        print("Goodbye")
+        
                 
                 
                 
